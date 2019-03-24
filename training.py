f333ae48cab2fb0fff5b16cd2afbc5464c51b655
Shubham Dixit <37299648+shubham-dixit@users.noreply.github.com>
2019-03-24 19:39:26 +0530

    Update training.py

--- training.py
+++ training.py
@@ -9,4 +9,8 @@ import numpy as np
 # Importing Python Image Library (PIL)
 from PIL import Image
 
+# Creating Local Binary Patterns Histograms for Face Recognition
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
+# Using prebuilt frontal face training model, for face detection
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
